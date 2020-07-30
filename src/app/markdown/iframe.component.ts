c8f280aee47b13c87a0f731597583d9bbc51c252
Alex Zhao <oatmonster@gmail.com>
2020-07-29 18:04:33 -0700

    Fixed embedded videos

--- src/app/markdown/iframe.component.ts
+++ src/app/markdown/iframe.component.ts
@@ -4,8 +4,8 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component( {
   selector: '<md-iframe>',
   template: `
-    <div class="embed-responsive embed-responsive-16by9 embed-video">
-      <iframe class="embed-responsive-item" [src]="safeUrl" allowfullscreen></iframe>
+    <div class="embeded-video">
+      <iframe [src]="safeUrl" allowfullscreen></iframe>
     </div>
   `,
   styleUrls: [ './block/block.component.scss' ]
