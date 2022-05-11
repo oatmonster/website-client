364aead3028f2bad23c545c09a0f953747b5168a
Alex Zhao <alexzhaowa@gmail.com>
2022-05-10 17:42:58 -0700

    Rolled back remark and unified to fix markdown rendering

--- src/app/markdown/markdown-factory.js
+++ src/app/markdown/markdown-factory.js
@@ -1,49 +1,35 @@
-( function ( global ) {
-	// 'use strict';
+// Imports the UNIFIED and REMARK modules
+import * as unified from 'unified';
+const parse = require( 'remark-parse' );
+const subsup = require( 'remark-sub-super' );
+const align = require( 'remark-align' );
+const iframes = require( 'remark-iframes' );
 
-	// Implements a basic process shim to support vfile / path modules to work in a browser
-	if ( !!global && !global.process ) {
+require( "regenerator-runtime/runtime" );
 
-		global.process = {
-			cwd: function () { return '/'; },
-			platform: ''
-		};
+const iframeConfig = {
+	'www.youtube.com': {
+		replace: [
+			[ 'watch?v=', 'embed/' ],
+			[ 'http://', 'https://' ],
+		],
+		thumbnail: {
+			format: 'http://img.youtube.com/vi/{id}/0.jpg',
+			id: '.+/(.+)$'
+		},
+		removeAfter: '&'
 	}
+}
 
-	// Imports the UNIFIED and REMARK modules
-	const unified = require( 'unified' );
-	const parse = require( 'remark-parse' );
-	const subsup = require( 'remark-sub-super' );
-	const align = require( 'remark-align' );
-	const iframes = require( 'remark-iframes' );
+// Setupd the markdown parser configured with the given options @see {https://github.com/remarkjs/remark/tree/master/packages/remark-parse}
+function markdownFactory() {
+	return unified()
+		.use( parse )
+		.use( subsup )
+		.use( align )
+		.use( iframes, iframeConfig )
+		.freeze();
+}
 
-	require( "regenerator-runtime/runtime" );
-
-	const iframeConfig = {
-		'www.youtube.com': {
-			replace: [
-				[ 'watch?v=', 'embed/' ],
-				[ 'http://', 'https://' ],
-			],
-			thumbnail: {
-				format: 'http://img.youtube.com/vi/{id}/0.jpg',
-				id: '.+/(.+)$'
-			},
-			removeAfter: '&'
-		}
-	}
-
-	// Setupd the markdown parser configured with the given options @see {https://github.com/remarkjs/remark/tree/master/packages/remark-parse}
-	function markdownFactory() {
-		return unified()
-			.use( parse )
-			.use( subsup )
-			.use( align )
-			.use( iframes, iframeConfig )
-			.freeze();
-	}
-
-	// Exports the parser setup function
-	module.exports = { markdownFactory }
-
-}( window ) );
+// Exports the parser setup function
+export { markdownFactory }
